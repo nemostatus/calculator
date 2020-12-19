a28785e1355e682011c15bd89af8a26ad04a4f42
Matthew <mattcorralejo1@gmail.com>
2020-12-19 08:35:52 -0800

    learned that operators can't be passed as parameters so I need to use callback functions

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
 function information(number){
- console.log(document.getElementById("main").value= document.getElementById("main").value + number)
+document.getElementById("main").value= document.getElementById("main").value + number
+}
+
+function calc(){
+    let el =document.getElementById("display")
+   console.log(el.value)
 }
